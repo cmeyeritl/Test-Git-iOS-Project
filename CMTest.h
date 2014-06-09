551d3786b67c4bd76ea8d0e5bd01efd4388dae90
Craig Meyer <ITL-Craig@mac.local>
2014-06-09 09:53:13 -0500

    added a test class

---
+++ CMTest.h
@@ -0,0 +1,13 @@
+//
+//  CMTest.h
+//  Git Test
+//
+//  Created by Craig Meyer on 6/9/14.
+//  Copyright (c) 2014 Craig Meyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CMTest : NSObject
+
+@end
---
+++ CMTest.m
@@ -0,0 +1,13 @@
+//
+//  CMTest.m
+//  Git Test
+//
+//  Created by Craig Meyer on 6/9/14.
+//  Copyright (c) 2014 Craig Meyer. All rights reserved.
+//
+
+#import "CMTest.h"
+
+@implementation CMTest
+
+@end
